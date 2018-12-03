1497495dabf26bf5b35f8b215cb56cf4f96c8b58
Kristof Van Tricht <kristofvantricht@gmail.com>
2018-12-03 21:02:22 +0100

    initial commit

---
+++ src/piweather/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from ._version import __version__
+import ._readers
+
+__all__ = ['__version__', '_readers']
---
+++ src/piweather/_readers.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+import os
+from piweather.sensors.read_bmp180 import *
