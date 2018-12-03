d6d2b1c9e40b87855627ae3ae40373075d548e5d
Kristof Van Tricht <kristofvantricht@gmail.com>
2018-12-03 22:24:06 +0100

    basic functionality

--- src/piweather/main.py
+++ src/piweather/main.py
@@ -1,9 +1,10 @@
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
-from piweather.sensors import read_bmp180
+from piweather.sensors import read_bmp180, read_ds18b20
 
 # Read BMP180
-(T_bmp180, P_bmp180) = read_bmp180.get_data()
-print('BMP180 Temperature = {} C'.format(T_bmp180))
-print('BMP180 Air pressure = {} hPa'.format(P_bmp180))
+read_bmp180.get_reading()
+
+# Read ds18b20
+read_ds18b20.get_all_readings()
